972b48084a32299494da7123a7c3d3ea39042d37
callmecposu <pc.cposu@gmail.com>
2024-02-19 23:37:11 -0500

    added a loading spinner for the current location

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,18 @@ export default function Home() {
     const [curLocErr, setCurLocErr] = useState<string | null>(null);
 
     useEffect(() => {
-       getCurrentLocInfo(setCurLocErr, setCurLocInfo)
+        getCurrentLocInfo(setCurLocErr, setCurLocInfo);
     }, []);
 
     return (
         <div>
             <Header></Header>
-            {curLocInfo && (
+            {curLocInfo ? (
                 <FullWeatherInfo info={curLocInfo}></FullWeatherInfo>
+            ) : (
+                <div className="flex h-screen justify-center items-center">
+                    <div className="w-16 loading loading-spinner text-primary"></div>
+                </div>
             )}
         </div>
     );
