a8916c8eebfdeaed0496c05905f0db99ef0ee626
callmecposu <pc.cposu@gmail.com>
2024-02-20 19:14:53 -0500

    added student declaration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* BTI425 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Maksym Volkovynskyi   Student ID: 126867225   Date: 2024/02/20
+*
+*
+********************************************************************************/
+
 import FullWeatherInfo from "@/components/fullWeatherInfo";
 import { useEffect, useState } from "react";
 import getCurrentLocInfo from "@/utils/getCurrentLocInfo";
